e70b38306951ca32acaf662ae51519ae7d99aa65
Xing9527 <929885647@qq.com>
2018-04-27 10:56:00 +0800

    修改了数据库名称

--- database/config.js
+++ database/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/party');
+mongoose.connect('mongodb://localhost/e_home');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
